a6ba69535902fea732af26324c0553917bbdd48f
Raphael <raphaeldejesus5@gmail.com>
2026-06-25 21:42:23 -0300

    fix: use client must be first on sugestao page

--- src/app/escala/sugestao/page.tsx
+++ src/app/escala/sugestao/page.tsx
@@ -1,6 +1,5 @@
-export const dynamic = 'force-dynamic'
-
 'use client'
+export const dynamic = 'force-dynamic'
 
 import { useState, useEffect } from 'react'
 import { createClient } from '@/lib/supabase/client'
@@ -341,4 +340,5 @@ export default function SugestaoPage() {
     </div>
   )
 }
+
 
